4177c62bbdaaf67f94221987b937fd79019ed3fb
ML-Workspace <amit.kumarsit2016@gmail.com>
2022-11-14 14:26:41 +0530

    Post Method Added

--- api/main.py
+++ api/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI 
+from pydantic import BaseModel
+from typing import Optional
 
 app= FastAPI()
 
-@app.get('/')
+taxonomies=[]
+
+class Taxonomoy(BaseModel):
+    tid: int
+    tname: str
+    vid: str
+    is_parent: Optional[bool] = None
+
+@app.get('/get/taxonomy')
 def home():
-    return {"greetings" : "Welcome to the FastApi !!!"}
+    return taxonomies
+
+@app.post('/add/taxonomy')
+def add_taxonomy(data: Taxonomoy):
+    taxonomies.append(data.dict())
+    return {"message": taxonomies}
